d1bced8006d19e7101ea8df7753ecdf4a096ca63
Jasa Perancang Situs Web <105140961+jasaperancangsitusweb@users.noreply.github.com>
2023-01-26 14:55:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ window.onscroll = () =>{
 
  // aos
  AOS.init();
+
+
+ // langganan email
+// ---------- Langganan Email ----------
+const form = document.getElementById("my-form");
+var loaderSubEmail = document.querySelector('.loader');
+loaderSubEmail.style.display = 'none';
+
+async function handleSubmit(event){
+  event.preventDefault();  
+  loaderSubEmail.style.display = 'block';
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      alert('Sukses! Tunggu email balasan dari kami!');
+      loaderSubEmail.style.display = 'none';
+      form.reset();
+    }
+  })
+  .catch(error => {
+    alert('Gagal! Pastikan data yang Anda masukkan sudah benar!');
+    loaderSubEmail.style.display = 'none';
+    form.reset();
+  });
+}
+form.addEventListener("submit", handleSubmit);
+ 
